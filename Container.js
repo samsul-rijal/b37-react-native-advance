9ea4c30cd2f8d79288a35f4b0edc969e2d6d2222
Your Name <samsulr99@gmail.com>
2022-08-24 14:02:07 +0700

    update

--- Container.js
+++ Container.js
@@ -1,10 +1,13 @@
 import * as React from "react";
 
 //Import Navigation Container
+import {NavigationContainer} from '@react-navigation/native'
 
 // Import Stack Navigation
+import {createStackNavigator} from '@react-navigation/stack'
 
 // Import Theme Native Base
+import { theme, useTheme } from "native-base";
 
 // Import Screen
 import FormNativeBase from "./src/screens/formNativeBase";
@@ -12,8 +15,25 @@ import Hello from "./src/screens/hello";
 import IncDec from "./src/screens/incDec";
 
 // Create Stack Navigation
+let Stack = createStackNavigator()
 
 export default function Container() {
   // Init Theme
-  return; // Refactor Code Navigation
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions= {{
+          headerMode: "screen",
+          headerTintColor: "white",
+          headerStyle: {backgroundColor: theme.colors.primary["300"]}
+        }} 
+      >
+        <Stack.Screen name="Home" component={Hello} options={{title: "Hello Home"}} />
+        <Stack.Screen name="IncDec" component={IncDec} options={{title: "Increment Decrement"}} />
+      
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  )
 }
--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { Text, Box } from "native-base";
+import { TouchableOpacity } from "react-native";
 
 // Add Props in Hello({navigation})
-export default function Hello() {
+export default function Hello({navigation}) {
   return (
     <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
@@ -10,6 +11,9 @@ export default function Hello() {
       </Text>
 
       {/* Create Button */}
+      <TouchableOpacity onPress={() => navigation.navigate("IncDec")}>
+        <Text>Screen Increment Decrement</Text>
+      </TouchableOpacity>
     </Box>
   );
 }
