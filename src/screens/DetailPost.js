b44967463b3cf13f7a5a6b460e000b6a6eba99ec
Your Name <samsulr99@gmail.com>
2022-08-24 16:44:38 +0700

    update

--- src/screens/DetailPost.js
+++ src/screens/DetailPost.js
@@ -10,25 +10,73 @@ import {
 import { ListItem, Avatar } from "react-native-elements";
 
 // Import Axios
+import axios from 'axios'
+import { useState, useEffect } from "react";
 
-const PostDetail = (props) => {
+const PostDetail = ({route}) => {
   //init Props
+  console.log(route);
+
+  const {id, title, body} = route.params
 
   //Init State
+  const [comment, setComment] = useState([])
+  const [isLoading, setIsLoding] = useState(false)
 
-  // Create LifeCycle
-  //Function Exception
 
-  // Create Function to fetch
+  const getComment = async() => {
+    
+    try {
+      setIsLoding(true)
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+      console.log(response);
+      setComment(response.data)
+      setIsLoding(false)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getComment()
+  },[])
 
   //   Create Component List
+
+  const renderItem = ({item})=> {
+    return(
+      <ListItem>
+        <ListItem.Title>
+          {item.email}
+        </ListItem.Title>
+
+        <ListItem.Subtitle>
+          {`${item.name} - ${item.body}`}
+        </ListItem.Subtitle>
+
+      </ListItem>
+    )
+  }
+
   return (
     <View style={style.container}>
-      <Text h2 style={{ fontWeight: "bold" }}>
-        This Is Post Detail
-      </Text>
-      {/* Implement Axios Here */}
-      {/* Render Component List */}
+      
+    <Text>
+      {title}
+    </Text>
+    <Text>{body}</Text>
+    <Text>Comment</Text>
+
+    <FlatList 
+      data={comment}
+      renderItem={renderItem}
+      key={(item)=> item.id}
+      refreshing={isLoading}
+      onRefresh={getComment}
+    
+    />
+
     </View>
   );
 };
--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,4 +1,6 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import { View, Text, Button, StyleSheet, FlatList } from "react-native";
 import { ListItem, Avatar } from "react-native-elements";
 
@@ -6,13 +8,58 @@ import { ListItem, Avatar } from "react-native-elements";
 
 const Posts = (props) => {
   //Init State
-
-  // Create LifeCycle
-  //Function Exception
+  const [post, setPost] = useState([])
+  const [isLoading, setIsLoding] = useState(false)
 
   // Create Function to fetch
+  const getPost = async() => {
+    
+    try {
+      setIsLoding(true)
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      console.log(response);
+      setPost(response.data)
+      setIsLoding(false)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getPost()
+  },[])
 
   //   Create Component List
+
+  const renderItem = ({item}) => {
+
+    return (
+      <ListItem
+        key={item.id}
+        onPress={()=> props.navigation.navigate("DetailPost", item)}
+        bottomDivider
+      >
+        <Avatar 
+          rounded
+          title={item.title.slice(0,2)}
+          containerStyle={{backgroundColor:"black"}}
+        />
+
+        <ListItem.Content>
+          <ListItem.Title h4 numberOfLines={1}>
+            {item.title}
+          </ListItem.Title>
+
+          <ListItem.Subtitle numberOfLines={2}>
+            {item.body}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+
+      </ListItem>
+    )
+  }
+
   return (
     <View style={style.container}>
       <View>
@@ -23,6 +70,16 @@ const Posts = (props) => {
           onPress={() => props.navigation.navigate("DetailPost")}
         />
         {/* Render Component List */}
+        <FlatList 
+          data={post}
+          renderItem={renderItem}
+          keyExtractor={(item)=> item.id}
+          refreshing={isLoading}
+          onRefresh={getPost}
+        />
+
+
+
       </View>
     </View>
   );
